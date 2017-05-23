b4cb61615339636cd7ba4a276e07cd2fbff3a833
Alexander Eimer <alexander.eimer@gmail.com>
2017-05-23 23:35:42 +0200

    Adds log-call everywhere

--- index.php
+++ index.php
@@ -22,10 +22,18 @@ $configLogFile = getenv( ENV_LOG_FILE );
 
 // Init Logger
 $logFile = null;
-if ($configFile !== null && is_string($configFile) && strlen($configFile) > 0) {
+if ( $configFile !== null && is_string( $configFile ) && strlen( $configFile ) > 0 ) {
 	$logFile = $configFile;
 }
-$log = new Logger($logFile);
+$log = new Logger( $logFile );
+$log->log( 'Logger initialised' );
+//$ip = getenv( 'HTTP_CLIENT_IP' ) ?:
+//	getenv( 'HTTP_X_FORWARDED_FOR' ) ?:
+//		getenv( 'HTTP_X_FORWARDED' ) ?:
+//			getenv( 'HTTP_FORWARDED_FOR' ) ?:
+//				getenv( 'HTTP_FORWARDED' ) ?:
+//					getenv( 'REMOTE_ADDR' );
+//$log->log( '+ Request from IP ' . $ip . ' at ' . date('d.m.Y H:i:s'));
 
 // Read config file
 $configContent = file_get_contents( $configFile );
@@ -33,6 +41,7 @@ $configJson    = json_decode( $configContent, true );
 
 // Check if config file was loaded successfully
 if ( ! $configFile || ! $configContent || $configJson === null ) {
+	$log->log( '+ ERR: CONFIG_FILE_NOT_FOUND' );
 	http_response_code( 500 );
 	die( 'CONFIG_FILE_NOT_FOUND' );
 }
@@ -42,10 +51,12 @@ $tasks = [];
 foreach ( $configJson['config'] as $task ) {
 	if ( array_key_exists( $task[ CONFIG_KEY ], $tasks ) ) {
 		// Check if no duplicate keys exist
+		$log->log( '+ ERR: DUPLICATE_KEY_ERROR' );
 		http_response_code( 500 );
 		die( 'DUPLICATE_KEY_ERROR' );
 	} elseif ( strlen( $task[ CONFIG_KEY ] ) < 10 ) {
 		// Check if the key is long enough
+		$log->log( '+ ERR: KEY_TOO_SHORT_ERROR' );
 		http_response_code( 500 );
 		die( 'KEY_TOO_SHORT_ERROR' );
 	}
@@ -54,6 +65,7 @@ foreach ( $configJson['config'] as $task ) {
 
 // Check post-param
 if ( ! isset( $_POST[ POST_PARAM_KEY ] ) || $_POST[ POST_PARAM_KEY ] === '' ) {
+	$log->log( '+ ERR: PARAMETER_KEY_NOT_FOUND' );
 	http_response_code( 400 );
 	die( 'PARAMETER_KEY_NOT_FOUND' );
 }
@@ -61,13 +73,18 @@ $taskKey = $_POST[ POST_PARAM_KEY ];
 
 // Key not found in config
 if ( ! key_exists( $taskKey, $tasks ) ) {
+	$log->log( '+ ERR: WRONG KEY' );
 	http_response_code( 401 );
 	die( 'WRONG KEY' );
 }
 
 // Execute command
 // The 2>&1 pipes the stderr stream to stdout, so we can see if there's an error
-echo shell_exec( $tasks[ $taskKey ] . ' 2>&1' );
+$shellReturn = shell_exec( $tasks[ $taskKey ] . ' 2>&1' );
+echo $shellReturn;
+$log->log( "+ SHELL RETURN:\n" . $shellReturn );
+
+$log->log( '+ SUCCESSFUL ENDED' );
 
 
 // Logger class for simpler logging
